e5de7260e80b153f418954c407f8fb5a723e97b2
Mauricio Martinez <mauricio.martinez1321@alumnos.udg.mx>
2020-09-20 22:31:12 -0500

    Declaramos la estructura personaje

---
+++ main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+#define MAX 100
+
+int _numeros[4];
+void mostrar();
+char arreglo[];
+void jugador();
+typedef struct personaje{char nombre[MAX];
+                         char tipo[MAX];
+                         int  fuerza;
+                         int  salud;}personaje;
+
+
+ int main(){
+
+    jugador();
+
+    getch();
+
+    printf("\n\n\t\tVuelva pronto\n\n\n\n");
+
+    return 0;
+}
+
+void jugador(){
+
+
+
+
+}
